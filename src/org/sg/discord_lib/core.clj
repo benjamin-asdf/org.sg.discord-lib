4173f2feb48897f02a7029c8a78c5a4ab90edeaf
Benj <Benjamin.Schwerdtner@gmail.com>
2022-02-07 13:47:08 +0100

    Add fn

---
+++ src/org/sg/discord_lib/core.clj
@@ -0,0 +1,12 @@
+(ns src.org.sg.discord-lib.core)
+
+(defn
+  msg-ref
+  [e
+   {:keys [channel-id guild-id id]}]
+  (assoc
+   e
+   :message-reference
+   {:channel_id channel-id
+    :guild_id guild-id
+    :message_id id}))
